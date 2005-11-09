c842c2accd037f63c6761b5d57c0fc409a5f430c
Stephen McCamant <smcc@alum.mit.edu>
2005-11-09 23:04:19 +0000

    Implement more of the fake iostreams library. This is now enough to get eon to compile, though the semantics are probably not right.
    CVS-ID: fake-libc-inc/fstream.h 1.2
    CVS-ID: fake-libc-inc/iostream.h 1.2

--- fake-libc-inc/fstream.h
+++ fake-libc-inc/fstream.h
@@ -3,10 +3,51 @@
 #ifndef PITTSFIELD_FSTREAM_H_INCLUDED
 #define PITTSFIELD_FSTREAM_H_INCLUDED
 
+#ifndef IS_THE_LIBRARY
 #include <stdio.h>
+#include <iostream.h>
+#endif
 
-class ifstream : public istream {};
+class ifstream : public istream {
+ public:
+    ifstream() {}
 
-class ofstream : public ostream {};
+    ifstream(int fd) {
+	fp = fdopen(fd, "r");
+    }
+
+    ifstream(const char *name) {
+	fp = fopen(name, "r");
+    }
+
+    void open(const char *name, int mode=ios::in) {
+        fp = fopen(name, "r");
+    }
+
+    void close(void) {
+	fclose(fp);
+    }
+};
+
+class ofstream : public ostream {
+ public:
+    ofstream() {}
+
+    ofstream(int fd) {
+	fp = fdopen(fd, "w");
+    }
+
+    ofstream(const char *name) {
+	fp = fopen(name, "w");
+    }
+
+    void open(const char *name, int mode=ios::out) {
+        fp = fopen(name, "w");
+    }
+
+    void close(void) {
+	fclose(fp);
+    }
+};
 
 #endif
--- fake-libc-inc/iostream.h
+++ fake-libc-inc/iostream.h
@@ -3,7 +3,9 @@
 #ifndef PITTSFIELD_IOSTREAM_H_INCLUDED
 #define PITTSFIELD_IOSTREAM_H_INCLUDED
 
+#ifndef IS_THE_LIBRARY
 #include <stdio.h>
+#endif
 
 class ios {
 public:
@@ -17,25 +19,87 @@ public:
         noreplace = 64,
         bin = 128,
         binary = 128 };
+    enum { skipws=01 };
 };
 
 class istream;
 class ostream;
 
 typedef ostream& (*omanip)(ostream&);
+typedef ssize_t streamsize;
+typedef unsigned long fmtflags;
 
 class istream {
+private:
+    void skip_ws() {
+	for (;;) {
+	    int c = getc(fp);
+	    if (c == EOF) {
+		return;
+	    } else if (!isspace(c)) {
+		ungetc(c, fp);
+		return;
+	    }
+	}
+    }
 public:
     istream() : failed(false) { }
     FILE *fp;
     bool failed;
-    istream& operator>>(char*);
-    istream& operator>>(char& c);
-    istream& operator>>(int&);
-    istream& operator>>(float&);
-    istream& operator>>(double&);
-    istream& get(char& c);
+    istream& operator>>(char *cp) {
+	fscanf(fp, " %s", cp);
+	return *this;
+    }
+    istream& operator>>(char& cr) {
+	char c;
+	fscanf(fp, " %c", &c);
+	cr = c;
+	return *this;
+    }
+    istream& operator>>(unsigned char& cr) {
+	unsigned char c;
+	fscanf(fp, " %c", (char *)&c);
+	cr = c;
+	return *this;
+    }
+    istream& operator>>(int& v) {
+	int x;
+	fscanf(fp, " %d", &x);
+	v = x;
+	return *this;
+    }
+    istream& operator>>(float& v) {
+	float f;
+	fscanf(fp, " %f", &f);
+	v = f;
+	return *this;
+    }
+    istream& operator>>(double& v) {
+	double f;
+	fscanf(fp, " %lf", &f);
+	v = f;
+	return *this;
+    }
+    int eof() const {
+	return feof(fp);
+    }
+    istream& get(char& c) {
+	int ch = fgetc(fp);
+	if (ch == EOF) {
+	    /* XXX do nothing? */
+	} else {
+	    c = ch;
+	}
+	return *this;
+    }
     istream& get(unsigned char& c) { return get((char&)c); }
+    istream& putback(char ch) {
+	ungetc(ch, fp);
+	return *this;
+    }
+    fmtflags unsetf(fmtflags mask) { return 0; /* XXX */ } 
+    int good() const { return !failed; }
+    int fail() const { return failed; }
     operator void*() const { return failed ? (void*)0 : (void*)(-1); }
     int operator!() const { return failed; }
 };
@@ -45,18 +109,56 @@ public:
     ostream() : failed(false) { }
     FILE *fp;
     bool failed;
-    ostream& operator<<(const char *s);
-    ostream& operator<<(int n);
-    ostream& operator<<(unsigned int n);
-    ostream& operator<<(long n);
-    ostream& operator<<(unsigned long n);
+    int good() const { return !failed; }
+    int fail() const { return failed; }
+    ostream& put(char c) {
+	putc(c, fp);
+	return *this;
+    }
+    fmtflags unsetf(fmtflags mask);
+    ostream& write(const char *s, streamsize n) {
+	fwrite(s, n, 1, fp);
+	return *this;
+    }
+    ostream& operator<<(const char *s) {
+	fputs(s, fp);
+	return *this;
+    }
+    ostream& operator<<(int n) {
+	fprintf(fp, "%d", n);
+	return *this;
+    }
+    ostream& operator<<(unsigned int n) { 
+	fprintf(fp, "%ud", n);
+	return *this;
+    }
+    ostream& operator<<(long n) {
+	fprintf(fp, "%ld", n);
+	return *this;
+    }
+    ostream& operator<<(unsigned long n) {
+	fprintf(fp, "%uld", n);
+	return *this;
+    }
     ostream& operator<<(short n) {return operator<<((int)n);}
     ostream& operator<<(unsigned short n) {return operator<<((unsigned int)n);}
     ostream& operator<<(bool b) { return operator<<((int)b); }
-    ostream& operator<<(double n);
-    ostream& operator<<(float f);
-    ostream& operator<<(long double n);
-    ostream& operator<<(const void *p);
+    ostream& operator<<(double n) {
+	fprintf(fp, "%g", n);
+	return *this;
+    }
+    ostream& operator<<(float f) {
+	fprintf(fp, "%g", f);
+	return *this;
+    }
+    ostream& operator<<(long double n) {
+	fprintf(fp, "%lg", n);
+	return *this;
+    }
+    ostream& operator<<(const void *p) {
+	fprintf(fp, "%p", p);
+	return *this;
+    }
     ostream& operator<<(omanip func) { return (*func)(*this); }
     operator void*() const { return failed ? (void*)0 : (void*)(-1); }
     int operator!() const { return failed; }
