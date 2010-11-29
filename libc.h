02edd8d918d627b92beaedc50cd9908262bbc114
Stephen McCamant <smcc@alum.mit.edu>
2010-11-29 03:06:14 +0000

    Redefine va_start to use the __builtin macro name that's apparently more standard across GCC versions.
    CVS-ID: libc.h 1.17

--- libc.h
+++ libc.h
@@ -686,7 +686,7 @@ REPLACEMENT int labs(long x);
 
 /* stdarg */
 
-#define va_start(v,l)   __builtin_stdarg_start((v),l)
+#define va_start(v,l)   __builtin_va_start((v),l)
 #define va_end          __builtin_va_end
 #define va_arg          __builtin_va_arg
 #define va_copy(d,s)    __builtin_va_copy((d),(s))
